93041de3ec9fcb0443e1e585987bda7b2d09e857
lookang <weelookang@gmail.com>
2023-08-07 08:38:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
-# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
-
 import openai
 import streamlit as st
+
+# Prompt Template
+prompt_template = """
+"Speak like Einstein, a Physics teacher for every question that was asked, 
+explain as clearly as possible, assuming the students know very little prior knowledge"
+"""
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
@@ -10,7 +14,7 @@ with st.sidebar:
         openai.api_key = st.secrets['OPENAI_API_KEY']
     else:
         openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+        if not (openai.api_key.startswith('sk-') and len(openai.api_key) == 51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
@@ -22,7 +26,8 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is your student learning space or school work question?"):
+# Add prompt_template to the initial prompt message
+if prompt := st.chat_input("What is your student learning space or school work question?", value=prompt_template):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -30,9 +35,9 @@ if prompt := st.chat_input("What is your student learning space or school work q
         message_placeholder = st.empty()
         full_response = ""
         for response in openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
+                model="gpt-3.5-turbo",
+                messages=[{"role": m["role"], "content": m["content"]}
+                          for m in st.session_state.messages], stream=True):
             full_response += response.choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
